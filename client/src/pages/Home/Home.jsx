c899ef4f294443aab3d4229f7ee3966458347f9d
David Barcenas <dabarcenas6921@gmail.com>
2022-09-17 23:49:15 -0400

    created sidebar and poetry cards

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,28 +1,169 @@
-import React from 'react'
-import { Navbar, Footer } from '../../components/index'
+import React from "react";
+import { Navbar, Footer } from "../../components/index";
 
 export default function Home() {
   return (
     <>
-    <Navbar location={location.pathname}/>
-    <Content/>
-    <Footer/>
+      <Navbar location={location.pathname} />
+      <Content />
+      <Footer />
     </>
-  )
+  );
 }
 
-function Content(){
-  return(
-    <div className='md:w-[1500px] sm:w-[500px] w-[350px] mt-[-96px] h-screen mx-auto text-left flex flex-col  justify-center '> 
-   
-    </div>
-  )
-}
-
-function Post(){
+function Content() {
   return (
-    <div>
-      
+    <div className="max-w-max mx-auto">
+      <Sidebar />
+      <Post />
     </div>
-  )
+  );
+}
+
+function Post() {
+  return (
+    <div className="w-[250px] rounded-lg border border-gray-200 hover:bg-gray-100 shadow-md bg-white p-3.5">
+      <div className="font-bold">Upon a Child That Died</div>
+      <p className="font-light text-left">
+        Here she lies, a pretty bud, <br /> Lately made of flesh and blood,{" "}
+        <br /> Who as soon fell fast asleep <br /> As her little eyes did peep.{" "}
+        <br /> Give her strewings, but not stir <br />
+        The earth that lightly covers her.
+      </p>
+      <div class="flex items-center space-x-1 mt-4">
+        <div class="flex-shrink-0">
+          <img
+            class="w-7 h-7 rounded-full"
+            src="https://lh6.googleusercontent.com/-UYKv4Oo4AL4/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3reOcBoiruxfuiRHs8VAoE-HvgnBDw/mo/photo.jpg?sz=256"
+            alt="Neil image"
+          />
+        </div>
+        <div class="flex-1">
+          <p class="text-sm font-semibold">Jane Doe</p>
+        </div>
+
+        <div>
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Sidebar() {
+  return (
+    <aside class="w-64" aria-label="Sidebar">
+      <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded">
+        <ul class="space-y-2">
+          <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                ></path>
+              </svg>
+              <span class="ml-3">Home</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                ></path>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                ></path>
+              </svg>
+              <span class="flex-1 ml-3 whitespace-nowrap">Popular</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                ></path>
+              </svg>
+              <span class="flex-1 ml-3 whitespace-nowrap">All Categories</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                ></path>
+              </svg>
+              <span class="flex-1 ml-3 whitespace-nowrap">My Favorites</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
 }
--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,45 +1,48 @@
-import React from 'react'
-import Typed from 'react-typed'
-import { Footer, Navbar, Button} from "../../components/index"
+import React from "react";
+import Typed from "react-typed";
+import { Footer, Navbar, Button } from "../../components/index";
 
 export default function Landing() {
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Footer/>
+      <Navbar />
+      <Hero />
+      <Footer />
     </>
-  )
+  );
 }
-
 
 const Hero = () => {
   return (
     <div>
-      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl h-max font-bold md:py-6 text-transparent bg-clip-text bg-gradient-to-r to-[#3626A7] from-[#1F255B]'>
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <h1 className="md:text-7xl sm:text-6xl text-4xl h-max font-bold md:py-6 text-transparent bg-clip-text bg-gradient-to-r to-[#3626A7] from-[#1F255B]">
           Write, Read, and Share Poems!
         </h1>
-        <div className='flex justify-center items-center'>
-          <p className='text-[#3626A7] md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-          Share your favorite 💌
+        <div className="flex justify-center items-center">
+          <p className="text-[#3626A7] md:text-5xl sm:text-4xl text-xl font-bold py-4">
+            Share your favorite 💌
           </p>
           <Typed
-          className='text-[#8375e7] md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['Haikus', 'Sonnets', 'Elegies', 'Ballads', 'Free Verses']}
+            className="text-[#8375e7] md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+            strings={["Haikus", "Sonnets", "Elegies", "Ballads", "Free Verses"]}
             typeSpeed={120}
             backSpeed={90}
             loop
           />
         </div>
-        <p className='text-[#3626A7] md:text-2xl text-xl font-bold'>Interested in poetry? Want to read poems? Thinking of writing poems?</p>
-        <ul className='md:flex justify-center'>
-        <li className='p-1'> <Button text="Start Reading"/></li>
-        <li className='p-1'> <Button text="Start Writing"/></li>
-      </ul>
-
+        <p className="text-[#3626A7] md:text-2xl text-xl font-bold">
+          Interested in poetry? Want to read poems? Thinking of writing poems?
+        </p>
+        <ul className="md:flex justify-center">
+          <li className="p-1">
+            <Button text="Start Reading" link="/signup" />
+          </li>
+          <li className="p-1">
+            <Button text="Start Writing" link="/signup" />
+          </li>
+        </ul>
       </div>
     </div>
   );
 };
-
